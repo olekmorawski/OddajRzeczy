450b27ff76fa80c0084ffd45a642d491f909ded7
Aleksander Morawski <olek.morawski@gmail.com>
2023-09-05 19:47:30 +0200

    more scss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,10 @@ const Home = () => {
       <div className="bg">
         <div className="nav">
           <div className="nav_links">
-            <Link to="/login">Log in</Link>
-            <Link to="/signup">Sign up</Link>
+            <div className="auth_links">
+              <Link to="/login">Log in</Link>
+              <Link to="/signup">Sign up</Link>
+            </div>
             <Link to="/">Start</Link>
             <Link to="/">O co chodzi?</Link>
             <Link to="/">O nas</Link>
@@ -17,10 +19,16 @@ const Home = () => {
           </div>
         </div>
         <div className="header">
-          <h1>Zacznij pomagac! Oddaj niechciane rzeczy w zaufane rece.</h1>
-          <div className="buttons">
-          <button>ODDAJ RZECZY</button>
-          <button>ZORGANIZUJ ZBIORKE</button>
+          <div className="text-and-buttons">
+            <h1>Zacznij pomagac! Oddaj niechciane rzeczy w zaufane rece.</h1>
+            <div className="buttons">
+              <button>
+                <span>ODDAJ RZECZY</span>
+              </button>
+              <button>
+                <span>ZORGANIZUJ ZBIÓRKĘ</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
