2cd61acee2c3875003c91e098c9890c6f7257f81
Aleksander Morawski <olek.morawski@gmail.com>
2023-09-05 15:54:03 +0200

    export + assets added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
